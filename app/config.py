8ca788a675029e3af367275665706c2568deac23
davisocoloski1 <davisocoloski1@gmail.com>
2025-04-20 01:02:30 -0300

    this is a new project of mine, i started coding config.py, setting the configuration that should be used all along the project, it is still in development and should get updates soon

---
+++ app/config.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    PROJCT_NAME: str = "Task API"
+    DEBUG: bool = bool(os.getenv("DEBUG", False))
+
+    # Databse connection (PostgreSQL - Sync with psycopg2)
+    DB_USER: str = os.getenv("POSTGRES_USER")
+    DB_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DATABASE_URL: str = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    # Auth (JWT)
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    if not SECRET_KEY:
+        raise ValueError("ERROR: SECRET_KEY is missing in the .env file!")
+
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+# Instantiate the config
+settings = Settings()
